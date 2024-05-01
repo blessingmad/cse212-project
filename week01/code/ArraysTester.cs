ac3fbc9751a3d36ed7fdf7f38ed9a109ff06e99c
blessingmad <mad22008@byui.edu>
2024-05-01 23:15:14 +0200

    dynamic array added

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -35,11 +35,16 @@ public static class ArraysTester {
     private static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // The variaable result will be used to store the output to be displayed at the console
+        //the for loop loops the int i which is greater than 0 for a number of times represented by the parameter length.
+        // i++ shows the increase of the number multiplied by.
+        var result = new double[ length];
+            for(int i = 0 ;  i < length; i++)
+            {
+                result [i] = number * (i + 1);
+            }
 
-        return new double[0]; // replace this return statement with your own
+        return result; //return the results
     }
     
     /// <summary>
@@ -53,9 +58,13 @@ public static class ArraysTester {
     private static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        //the for loop loops the data in i times and inserts a new number greater than 0 
+        //the loop rotates the numbers inserted and remove 
+        for (int i = 0; i < amount; i++){
+            data.Insert (0, data [1]);
+            data.Remove(data.Count - 1);
+        }
+        
 
     }
 }
